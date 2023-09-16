bc11163a2e900039b304b4ba798b68550603fa9c
z22756392z <79678055+z22756392z@users.noreply.github.com>
2023-09-16 16:40:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ var commentData;
 function init(){
     //fetch the json data we need
     
-    
-    setTimeout( () => {//just want to see the loading screen
+    ```
+    setTimeout(() => {//just want to see the loading screen
         Fetch()
         .then(newData => {//sucess
             commentData = newData.comment;
             navbar.show();
             Home.show();
-        })//.catch(err => {//failed
-            //error.fatalError(err);
-    //})
-    },1000);
+        }).catch(err => {//failed
+            error.fatalError(err);
+    })},1000);
+    ```
+    setTimeout(( () => {
+        navbar.show();
+        Home.show();
+    }), 1000);
     
     isLoading.show();
 }
